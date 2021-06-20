8ce6b164de6a68b8f0860fec9dcda11a0a4de3eb
武志祥 <657873584@qq.com>
2021-06-20 08:43:37 +0800

    屏蔽查看源码

--- public/js/disable.js
+++ public/js/disable.js
@@ -1,9 +1,9 @@
-// $(document).on('keydown', function (e) {
-//     e = e.event || window.event;
-//     if (e.keyCode === 123 || (e.ctrlKey && e.shiftKey && e.keyCode === 73) || (e.ctrlKey && e
-//         .keyCode === 85)) return false
-// })
-//
-// $(document).bind("contextmenu", function (e) {
-//     return false;
-// });
+$(document).on('keydown', function (e) {
+    e = e.event || window.event;
+    if (e.keyCode === 123 || (e.ctrlKey && e.shiftKey && e.keyCode === 73) || (e.ctrlKey && e
+        .keyCode === 85)) return false
+})
+
+$(document).bind("contextmenu", function (e) {
+    return false;
+});
